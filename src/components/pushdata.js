e1db3a61755c61fe172668f4cb26b49eb22489fd
Andy Park <apark1214@gmail.com>
2021-11-30 19:47:30 -0800

    testing data for upload

---
+++ src/components/pushdata.js
@@ -0,0 +1,5 @@
+import algoliasearch from 'algoliasearch';
+import data from '../../data/products.json';
+
+console.log(data);
+// const client = algoliasearch('VYLEWMPKEZ', '8940a18fde155adf3f74b0912c267aa4');
